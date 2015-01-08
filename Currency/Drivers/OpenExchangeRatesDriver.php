859ebff2d6b99bd26473aaa61097adf61411f6be
Mantas Jonusas <mantas.jonusas@nfq.lt>
2015-01-08 17:13:34 +0200

    added GuzzleHttp Client and updated OpenExchangeRatesDriver namespace

--- Currency/Drivers/OpenExchangeRatesDriver.php
+++ Currency/Drivers/OpenExchangeRatesDriver.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace ONGR\CurrencyExchangeBundle\Currency\CurrencyExchange;
+namespace ONGR\CurrencyExchangeBundle\Currency\Drivers;
 
 use ONGR\CurrencyExchangeBundle\Currency\CurrencyDriverInterface;
 use GuzzleHttp\Client;
