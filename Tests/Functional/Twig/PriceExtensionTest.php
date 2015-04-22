f1c1c6a92a18df6e6cafc662d0eff1bedd6cfcac
Erik Maslak <emaslak@nfq.lt>
2015-04-22 11:09:39 +0300

    Updated bundle to use strict standard ~2.1.

--- Tests/Functional/Twig/PriceExtensionTest.php
+++ Tests/Functional/Twig/PriceExtensionTest.php
@@ -11,7 +11,6 @@
 
 namespace ONGR\CurrencyExchangeBundle\Tests\Functional\Twig;
 
-use ONGR\CurrencyExchangeBundle\Service\CurrencyExchangeService;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use ONGR\CurrencyExchangeBundle\Twig\PriceExtension;
 
