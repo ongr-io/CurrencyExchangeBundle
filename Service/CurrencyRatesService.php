4b44a3ce3de825dea5ad35e9b4eb3f4c12a0786b
Mantas <marc.mantas@gmail.com>
2016-06-08 13:33:38 +0300

    implemented getRates() return rates for a specific date

--- Service/CurrencyRatesService.php
+++ Service/CurrencyRatesService.php
@@ -18,11 +18,13 @@ use ONGR\CurrencyExchangeBundle\Driver\CurrencyDriverInterface;
 use ONGR\CurrencyExchangeBundle\Exception\RatesNotLoadedException;
 use ONGR\ElasticsearchBundle\Result\Result;
 use ONGR\ElasticsearchDSL\Query\MatchAllQuery;
+use ONGR\ElasticsearchDSL\Query\RangeQuery;
 use ONGR\ElasticsearchDSL\Sort\FieldSort;
 use ONGR\ElasticsearchBundle\Service\Manager;
 use Psr\Log\LoggerAwareTrait;
 use Stash\Interfaces\ItemInterface;
 use Stash\Interfaces\PoolInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * This class provides currency rates.
@@ -69,46 +71,59 @@ class CurrencyRatesService
     /**
      * This method returns exchange rates.
      *
+     * @param string $date
+     *
      * @throws RatesNotLoadedException
      * @return array
      */
-    public function getRates()
+    public function getRates($date = '')
     {
-        if (isset($this->rates)) {
-            return $this->rates;
+        $date = $date == '' ? $this->getCurrentDate() : $date;
+
+        if (isset($this->rates[$date])) {
+            return $this->rates[$date];
         }
 
         $item = $this->getCachedRates();
         $this->rates = $item->get();
-        if (isset($this->rates)) {
-            return $this->rates;
+        if (isset($this->rates[$date])) {
+            return $this->rates[$date];
         }
 
         $this->rates = $this->getRatesFromBackup();
-        if (isset($this->rates)) {
-            return $this->rates;
+        if (isset($this->rates[$date])) {
+            return $this->rates[$date];
         }
 
-        $this->rates = $this->reloadRates();
-        if (isset($this->rates)) {
-            return $this->rates;
+        $this->reloadRates();
+        if (isset($this->rates[$date])) {
+            return $this->rates[$date];
         }
-        throw new RatesNotLoadedException('Currency rates are not loaded and could not be loaded on demand');
+        throw new RatesNotLoadedException('Currency rates for '.$date.' are not loaded and could not be loaded on demand');
     }
 
     /**
      * Returns currency rates from ES.
      *
+     * @param string $date
+     *
      * @return array
      */
-    private function getRatesFromBackup()
+    private function getRatesFromBackup($date = '')
     {
+        $date = $date == '' ? $this->getCurrentDate() : $date;
         $rates = [];
         $repository = $this->manager->getRepository('ONGRCurrencyExchangeBundle:CurrencyDocument');
         $search = $repository->createSearch();
-        $search->addSort(new FieldSort('created_at', FieldSort::DESC));
-        $query = new MatchAllQuery();
-        $search->addQuery($query);
+        $search->addFilter(
+            new RangeQuery(
+                'created_at',
+                [
+                    'gte' => $date.'T00:00:00',
+                    'lte'   => $date.'T23:59:59',
+                ]
+            )
+        );
         $search->setSize(1);
         try {
             $results = $repository->execute($search, Result::RESULTS_ARRAY);
@@ -123,9 +138,9 @@ class CurrencyRatesService
                 $rates[$data['name']] = $data['value'];
             }
             $this->logger && $this->logger->notice('Rates returned from ES. Cache updated.');
-            $this->updateRatesCache($rates);
+            $this->updateRatesCache($rates, new \DateTime($results[0]['created_at']));
 
-            return $rates;
+            return [$date => $rates];
         }
 
         return null;
@@ -134,11 +149,16 @@ class CurrencyRatesService
     /**
      * Update rates in cache.
      *
-     * @param array $rates
+     * @param array     $rates
+     * @param \DateTime $createdAt
      */
-    private function updateRatesCache($rates)
+    private function updateRatesCache($rates, \DateTime $createdAt = null)
     {
-        $this->getCachedRates()->set($rates);
+        $createdAt = $createdAt ? $createdAt : new \DateTime();
+        $cache = [
+            $createdAt->format('Y-m-d') => $rates
+        ];
+        $this->getCachedRates()->set($cache);
     }
 
     /**
@@ -156,13 +176,20 @@ class CurrencyRatesService
      */
     public function reloadRates()
     {
-        $this->rates = $this->driver->getRates();
+        $date = $this->getCurrentDate();
+        $rates = $this->driver->getRates();
+
+        if ($rates) {
+            $this->rates = [
+                $date => $rates
+            ];
+        }
 
         /** @var CurrencyDocument $document */
         $document = new CurrencyDocument();
         
-        if ($this->rates) {
-            foreach ($this->rates as $name => $value) {
+        if ($this->rates[$date]) {
+            foreach ($this->rates[$date] as $name => $value) {
                 $ratesObject = new RatesObject();
                 $ratesObject->setName($name);
                 $ratesObject->setValue($value);
@@ -170,7 +197,7 @@ class CurrencyRatesService
             }
             $this->manager->persist($document);
             $this->manager->commit();
-            $this->updateRatesCache($this->rates);
+            $this->updateRatesCache($this->rates, $document->getCreatedAt());
 
             return $this->rates;
         }
@@ -187,5 +214,16 @@ class CurrencyRatesService
     public function getBaseCurrency()
     {
         return $this->driver->getBaseCurrency();
+    }
+
+    /**
+     * Returns formatted current date
+     *
+     * @returns string
+     */
+    private function getCurrentDate()
+    {
+        $now = new \DateTime();
+        return $now->format('Y-m-d');
     }
 }
